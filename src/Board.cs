2a6a978f02e54141775d7e1b4c57f184ea1580ad
= <zerolake@tuta.io>
2021-10-05 21:16:24 -0400

    board structure and filling

--- src/Board.cs
+++ src/Board.cs
@@ -13,16 +13,25 @@ namespace Chess
         {
             Cells = new Cell[8][];
             
-            for (int i = 0; i < Cells.GetLength(0); i ++)
+            for (int i = 0; i < Cells.Length; i ++)
             {
                 Cells[i] = new Cell[8];
-
-                for (int j = 0; j < Cells.GetLength(1); j ++)
+                for (int j = 0; j < Cells[i].Length; j ++)
                 {
                     Cells[i][j] = new Cell();
                 }
             }
+        }
 
+        public void Print()
+        {
+            for (int i = 0; i < Cells.Length; i ++)
+            {
+                for (int j = 0; j < Cells[i].Length; j ++)
+                {
+                    Console.WriteLine($"Row: {i} Rank: {j} is equal to:\n{Cells[i][j]}");
+                }
+            }
         }
 
         // Cells is a jagged array of containing 8 sub arrays of 8 cells (8x8)
---
+++ src/Piece.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Chess
+{
+    class Piece
+    {
+           
+    }
+}
